76ac8d2889381b715828f82347571fb37819c25f
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2024-09-16 14:23:48 +0200

    config.ts changes for baseUrl issue #1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,12 +11,13 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus-play/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'akyriako', // Usually your GitHub org/user name.
+  projectName: 'docusaurus-play', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
