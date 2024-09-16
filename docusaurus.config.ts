188950a1dd68e420c4018417f6abae5791fe1881
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2024-09-16 14:02:39 +0200

    take #1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'My PR Preview Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
